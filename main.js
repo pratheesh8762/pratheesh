60fcc92be68299151b8b0af32f3e7fbc0074a4eb
pratheesh8762 <148521308+pratheesh8762@users.noreply.github.com>
2023-10-21 08:27:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["COLLEGE STUDENT","PROFFESIONAL PHOTOGRAPHY","PHOTO AND VIDEO EDITOR"],
+    typeSpeed:50,
+    backSpeed:20,
+    backdDelay:1000,
+    loop:true
+});
